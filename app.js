8e749f89135b062adaf2d71980b24170b06e43c1
Levi <levi_blodgett@outlook.com>
2019-02-10 22:05:00 -0600

    Added get and populate functionality to app and UI

--- app.js
+++ app.js
@@ -24,6 +24,9 @@ const ItemCtrl = (function() {
 
   // Publics methods
   return {
+    getItems: function() {
+      return data.items;
+    },
     logData: function() {
       return data;
     }
@@ -34,10 +37,29 @@ const ItemCtrl = (function() {
 
 // UI Controller
 const UICtrl = (function() {
+  const UISelectors = {
+    itemList: '#item-list'
+  }
   
   // Publics methods
   return {
+    populateItemList: function(items) {
+      let html = '';
 
+      items.forEach(function(item) {
+        html += `
+        <li class="collection-item" id="item-${item.id}">
+          <strong>${item.name}: </strong><em>${item.calories} Calories</em>
+          <a href="#" class="secondary-content">
+            <i class="edit-item fa fa-pencil"></i>
+          </a>
+        </li>
+        `;
+      });
+
+      // Insert list items
+      document.querySelector(UISelectors.itemList).innerHTML = html;
+    }
   }
 })();
 
@@ -48,7 +70,11 @@ const AppCtrl = (function(ItemCtrl, UICtrl) {
   // Public methods
   return {
     init: function() {
-      console.log('Initializing application....');
+      // Fetch items from data structure
+      const items = ItemCtrl.getItems();
+
+      // Populate list with items
+      UICtrl.populateItemList(items);
     }
   }
 })(ItemCtrl, UICtrl);
